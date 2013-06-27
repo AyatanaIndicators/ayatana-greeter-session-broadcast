68d2500481d275afbd8f6cc14d3f973eead5a7d5
Ted Gould <ted@gould.cx>
2013-06-27 09:15:32 -0500

    Implementing the function and cleaning up the header

---
+++ libunity-greeter-session-broadcast/unity-greeter-session-broadcast.c
@@ -0,0 +1,11 @@
+
+#include "unity-greeter-session-broadcast.h"
+
+void
+unity_session_greeter_broadcast_request_app_start (const gchar * username, const gchar * app_id)
+{
+
+
+	return;
+}
+
--- libunity-greeter-session-broadcast/unity-greeter-session-broadcast.h
+++ libunity-greeter-session-broadcast/unity-greeter-session-broadcast.h
@@ -1,7 +1,19 @@
 
 #include <glib.h>
 
+#ifndef __UNITY_GREETER_SESSION_BROADCAST_H__
+#define __UNITY_GREETER_SESSION_BROADCAST_H__ 1
+
+#pragma GCC visibility push(default)
+
+G_BEGIN_DECLS
+
 void
 unity_session_greeter_broadcast_request_app_start (const gchar * username,
                                                    const gchar * app_id);
 
+G_END_DECLS
+
+#pragma GCC visiblity pop
+
+#endif /* __UNITY_GREETER_SESSION_BROADCAST_H__ */
